b59ba1bff1d3cc7dd6b0b3acb6679fafc723154d
Markus Videfors <markus.videfors@internetstiftelsen.se>
2020-04-27 10:22:31 +0200

    Create basic bot subclass

--- bot.py
+++ bot.py
@@ -4,3 +4,8 @@ import sys
 import boto3
 import discord
 from discord.ext import commands
+
+
+class Bot(commands.bot.Bot):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
