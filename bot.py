dbd70a095813f6a6b2280f7c2ca2f3c9d00a76c9
Markus Videfors <markus.videfors@internetstiftelsen.se>
2020-04-27 10:23:54 +0200

    create serverstart command

--- bot.py
+++ bot.py
@@ -15,3 +15,19 @@ class Bot(commands.bot.Bot):
         print(self.user.name)
         print(self.user.id)
         print('------')
+
+    @commands.command()
+    async def startserver(self, ctx):
+        print(f"Recieved command startserver from user {ctx.message.author}")
+        ec2 = boto3.client("ec2")
+        print("Starting ec2 instance")
+        response = ec2.start_instances(
+            InstanceIds=[
+                'i-0986e940a442f36c0',
+            ]
+        )
+        print(
+            "Current state of instance: "
+            f"{response['StartingInstances'][0]['CurrentState']['Name']}"
+        )
+
