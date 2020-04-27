5b18d3a57a8ca242bc280594159a70f02215f537
Markus Videfors <markus.videfors@internetstiftelsen.se>
2020-04-27 10:22:49 +0200

    add on_ready

--- bot.py
+++ bot.py
@@ -9,3 +9,9 @@ from discord.ext import commands
 class Bot(commands.bot.Bot):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    async def on_ready(self):
+        print('Logged in as')
+        print(self.user.name)
+        print(self.user.id)
+        print('------')
